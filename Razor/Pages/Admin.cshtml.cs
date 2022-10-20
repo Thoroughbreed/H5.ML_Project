1a09bb79f5dcaf1eb861247c7ca02eee875f20c3
Jan Andreasen <jan@tved.it>
2022-10-20 21:12:24 +0200

    FAQ and Admin done

--- Razor/Pages/Admin.cshtml.cs
+++ Razor/Pages/Admin.cshtml.cs
@@ -1,11 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Razor.Pages;
 
 public class Admin : PageModel
 {
+    [BindProperty(SupportsGet = true)] public string Password { get; set; }
+    [BindProperty(SupportsGet = true)] public string Response { get; set; }
+
+    private HttpClient _client;
+
+    public Admin(HttpClient client)
+    {
+        _client = client;
+    }
+
     public void OnGet()
     {
-        
+    }
+
+    public async Task<IActionResult> OnPost()
+    {
+        if (Password != "potato")
+        {
+            Response = "Wrong password Nicolai!";
+            return Page();
+        }
+
+        Response = "Password accepted ...";
+        var res = await _client.GetAsync("/retrain");
+        Response = res.StatusCode.ToString();
+        var resCode = await res.Content.ReadAsStringAsync();
+        Response = resCode;
+        return Page();
     }
 }
