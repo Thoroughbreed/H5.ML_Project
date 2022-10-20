745e8f90e8966ae1f55b8e5b04a28ea65ad574be
Jan Andreasen <jan@tved.it>
2022-10-20 22:21:07 +0200

    Ready to release

--- Razor/Program.cs
+++ Razor/Program.cs
@@ -2,10 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddCors();
 // builder.Services.AddScoped(sp => 
 //     new HttpClient { BaseAddress = new Uri("http://62.66.208.26:8089") });
 builder.Services.AddScoped(sp => 
-    new HttpClient { BaseAddress = new Uri("https://192.168.2.10:7258") });
+    new HttpClient { BaseAddress = new Uri("http://192.168.2.10:8080") });
 
 // REMEMBER TO ADD THIS TO JSON
 // ;https://192.168.2.10:7175
@@ -20,10 +21,18 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseCors(o =>
+{
+    o.AllowAnyHeader();
+    o.AllowAnyMethod();
+    o.AllowAnyOrigin();
+});
 
 app.UseAuthorization();
 
